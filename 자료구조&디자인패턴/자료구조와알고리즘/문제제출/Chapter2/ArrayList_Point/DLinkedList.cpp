c664a786e9e80f03cd672eae1a2255b628c52da5
taeyong <album5th@naver.com>
2019-08-06 18:40:22 +0900

    DLinkedList (Point)

---
+++ 자료구조&디자인패턴/자료구조와알고리즘/문제제출/Chapter2/ArrayList_Point/DLinkedList.cpp
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "DLinkedList.h"
+
+void ListInit(List * plist)
+{
+	plist->head = (Node*)malloc(sizeof(Node));
+	plist->head->next = NULL;
+	plist->comp = NULL;
+	plist->numOfData = 0;
+}
+
+void FInsert(List * plist, LData data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+	newNode->data = data;
+
+	newNode->next = plist->head->next;
+	plist->head->next = newNode;
+
+	(plist->numOfData)++;
+}
+
+void SInsert(List * plist, LData data)
+{
+	Node * newNode = (Node*)malloc(sizeof(Node));
+	Node * pred = plist->head;
+	newNode->data = data;
+
+	while (pred->next != NULL &&
+		plist->comp(data, pred->next->data) != 0)
+	{
+		pred = pred->next;
+	}
+
+	newNode->next = pred->next;
+	pred->next = newNode;
+
+	(plist->numOfData)++;
+}
+
+
+void LInsert(List * plist, LData data)
+{
+	if (plist->comp == NULL) // 정렧기준이 설정되어 있지 않으면..(정렧 기준이 없을 수도 있다.)
+		FInsert(plist, data); // 머리에 Node를 추가!
+	else
+		SInsert(plist, data); // 정렧기준에 근거하여 Node를 추가!
+}
+
+int LFirst(List * plist, LData * pdata)
+{
+	if (plist->head->next == NULL)
+		return FALSE;
+
+	plist->before = plist->head;
+	plist->cur = plist->head->next;
+
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+int LNext(List * plist, LData * pdata)
+{
+	if (plist->cur->next == NULL)
+		return FALSE;
+
+	plist->before = plist->cur;
+	plist->cur = plist->cur->next;
+
+	*pdata = plist->cur->data;
+	return TRUE;
+}
+
+LData LRemove(List * plist)
+{
+	Node * rpos = plist->cur;
+	LData rdata = rpos->data;
+
+	plist->before->next = plist->cur->next;
+	plist->cur = plist->before;
+
+	free(rpos);
+	(plist->numOfData)--;
+	return rdata;
+}
+
+int LCount(List * plist)
+{
+	return plist->numOfData;
+}
+
+void SetSortRule(List * plist, int(*comp)(LData d1, LData d2))
+{
+	
+}
